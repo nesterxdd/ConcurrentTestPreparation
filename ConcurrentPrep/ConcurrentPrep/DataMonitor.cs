bc4c9e5dead49e2df83870428594a79d7619effb
nesterxdd <nesterknight7@gmail.com>
2024-10-24 16:23:51 +0300

    New Version

---
+++ ConcurrentPrep/ConcurrentPrep/DataMonitor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentPrep
+{
+    class DataMonitor
+    {
+        public string mystring { get; private set; }
+        object lockObject = new object();
+
+        public bool isDone { get; private set; } = false;
+        public int countA { get; private set; } = 0;
+        public int countB { get; private set; } = 0;
+        public int countC { get; private set; } = 0;
+
+        public DataMonitor()
+        {
+            mystring = "*";
+        }
+
+        private int AddChar(char character, int counter)
+        {     
+            mystring += character;
+            counter++;
+            if (counter >= 15)
+            {
+                isDone = true;
+                System.Threading.Monitor.PulseAll(lockObject);
+                return counter;
+            }
+            Monitor.PulseAll(lockObject);
+            return counter;
+        }
+
+        public void AddA()
+        {
+            lock (lockObject)
+            {
+                countA = AddChar('A', countA);
+            }
+        }
+
+        public void AddB()
+        {
+            lock (lockObject)
+            {
+                if (countA < 3)
+                {
+                    Monitor.Wait(lockObject);
+                }
+                countB = AddChar('B', countB);
+            }
+        }
+
+        public void AddC()
+        {
+            lock (lockObject)
+            {
+                if (countA < 3)
+                {
+                    Monitor.Wait(lockObject);
+                }
+                countC = AddChar('C', countC);
+            }
+        }
+    }
+}
--- ConcurrentPrep/ConcurrentPrep/Program.cs
+++ ConcurrentPrep/ConcurrentPrep/Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
+﻿using ConcurrentPrep;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 class Program
 {
-    static object lockObject = new object();
-    static string mystring = "";
-    static int countA = 0;
-    static int countB = 0;
-    static int countC = 0;
-    static int countVowels = 0;
+    
 
-    static bool isDone = false;
+    static DataMonitor dataMonitor = new DataMonitor();
     static void Main(string[] args)
     {
 
@@ -19,70 +15,43 @@ class Program
         Thread threadA = new Thread(ThreadA);
         Thread threadB = new Thread(ThreadB);
         Thread threadC = new Thread(ThreadC);
+        
 
-        Thread printThread = new Thread(PrintThread);
 
         threadA.Start();
         threadB.Start();
         threadC.Start();
-        printThread.Start();
+
+        while(dataMonitor.isDone == false)
+        {
+            Console.WriteLine(dataMonitor.mystring);
+        }
 
         threadA.Join();
         threadB.Join();
         threadC.Join();
 
-        printThread.Join();
 
-        Console.WriteLine("Final string: " + mystring);
-        Console.WriteLine($"CountA {countA} countB {countB} countC {countC}");
+        Console.WriteLine("Final string: " + dataMonitor.mystring);
+        Console.WriteLine($"CountA {dataMonitor.countA} countB {dataMonitor.countB} countC {dataMonitor.countC}");
     }
 
     public static void ThreadA()
     {
-
-        while (!isDone)
+        while (!dataMonitor.isDone)
         {
-            lock (lockObject)
-            {
-                mystring += "A";
-                countA++;
-                countVowels++;
-                if (countA >= 15)
-                {
-                    isDone = true;
-                    Monitor.PulseAll(lockObject);
-                    return;
-                }
-                
-                Monitor.PulseAll(lockObject);
-            }
+            dataMonitor.AddA();
             Thread.Sleep(100); // Simulate work with a short sleep
-
         }
     }
 
     public static void ThreadB()
     {
 
-        while (!isDone)
+        while (!dataMonitor.isDone)
         {
-            lock (lockObject)
-            {
-                while (countVowels < 3)
-                {
-                    Monitor.Wait(lockObject);
-                }
-                mystring += "B";
-                countB++;
-                if (countB >= 15)
-                {
-                    isDone = true;
-                    Monitor.PulseAll(lockObject);
-                    return;
-                }
 
-                Monitor.PulseAll(lockObject);
-            }
+            dataMonitor.AddB();
             Thread.Sleep(100); // Simulate work with a short sleep
         }
 
@@ -93,25 +62,9 @@ class Program
     public static void ThreadC()
     {
 
-        while (!isDone)
+        while (!dataMonitor.isDone)
         {
-            lock (lockObject)
-            {
-                while (countVowels <= 3)
-                {
-                    Monitor.Wait(lockObject);
-                }
-                mystring += "C";
-                countC++;
-                if (countC >= 15)
-                {
-                    isDone = true;
-                    Monitor.PulseAll(lockObject);
-                    return;
-                }
-                Monitor.PulseAll(lockObject);
-
-            }
+            dataMonitor.AddC();
             Thread.Sleep(100); // Simulate work with a short sleep
         }
 
@@ -119,16 +72,4 @@ class Program
 
     }
 
-    public static void PrintThread()
-    {
-        while (!isDone)
-        {
-            lock (lockObject)
-            {
-                Console.WriteLine(mystring);
-                Monitor.Wait(lockObject); 
-            }
-           
-        }
-    }
 }
